d0db31ea6624fed066afc5a730f8d100e1683f95
{quockiet} <{nqkiet111@gmail.com}>
2022-06-25 13:04:02 +0700

    server nut xac nhan update

---
+++ ServerAPI/Controllers/clsDatabase.cs
@@ -0,0 +1,44 @@
+﻿    using System.Data;
+using System.Data.SqlClient;
+
+
+
+namespace ServerAPI.Controllers
+
+
+{
+    public class clsDatabase
+    {
+        public static SqlConnection con;
+
+        public static bool OpenConnection()
+        {
+            try
+            {
+                con = new SqlConnection("Data Source=DESKTOP-NP8PEAO;Initial Catalog=QLMoiTruongDoThi; Integrated Security=true");
+                con.Open();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+        public static bool CloseConnection()
+        {
+            try
+            {
+                con.Close();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+
+
+    }
+}
